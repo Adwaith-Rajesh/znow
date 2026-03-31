69406a089848df986bf93a183fc0a2acf047845a
Adwaith-Rajesh <me@adwaith.dev>
2026-03-31 11:10:08 +0530

    [dev] basic implementation
    Implemented a threaded and a non threaded version.

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "zig",
+        .name = "znow",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
@@ -22,4 +22,16 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+
+    // simple tests
+    const znow_test = b.addTest(.{ .root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    }) });
+
+    const run_test = b.addRunArtifact(znow_test);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_test.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,79 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const WORKER_ID_BITS = 10;
+const SEQUENCE_BITS = 12;
+const MAX_SEQUENCE = ((1 << SEQUENCE_BITS) - 1);
+const WORKER_ID_SHIFT = SEQUENCE_BITS;
+const TIMESTAMP_SHIFT = WORKER_ID_BITS + SEQUENCE_BITS;
+
+fn Snowflake(comptime thread_safe: bool) type {
+    return struct {
+        io: std.Io,
+
+        worker_id: u16,
+
+        // Jan 2025 00:00:00 in ms
+        custom_epoch: u64 = 1735689600000,
+
+        sequence: u16 = 0,
+        last_timestamp: u64 = 0,
+        mutex: if (thread_safe) std.Io.Mutex else void = if (thread_safe) .init else {},
+
+        const Self = @This();
+
+        pub fn init(io: std.Io, worker_id: u16) Self {
+            return .{
+                .io = io,
+                .worker_id = worker_id,
+            };
+        }
+
+        inline fn nowMs(self: *const Self) u64 {
+            return @intCast(std.Io.Clock.now(.real, self.io).toMilliseconds());
+        }
+
+        fn waitNextMS(self: *const Self) u64 {
+            var curr_ts: u64 = self.nowMs();
+            while (curr_ts <= self.last_timestamp) {
+                curr_ts = self.nowMs();
+            }
+            return curr_ts;
+        }
+
+        pub fn next(self: *Self) !u64 {
+            if (thread_safe) try self.mutex.lock(self.io);
+            defer if (thread_safe) self.mutex.unlock(self.io);
+
+            var ts: u64 = self.nowMs();
+
+            if (ts == self.last_timestamp) {
+                self.sequence = (self.sequence + 1) & MAX_SEQUENCE;
+
+                if (self.sequence == 0) {
+                    ts = self.waitNextMS();
+                }
+            } else {
+                self.sequence = 0;
+            }
+            self.last_timestamp = ts;
+            return ((ts - self.custom_epoch) << TIMESTAMP_SHIFT) | (self.worker_id << WORKER_ID_SHIFT) | self.sequence;
+        }
+    };
 }
+
+test "Snowflake_no_threads" {
+    // is this test even necessary
+    var flake: Snowflake(false) = .init(std.testing.io, 12);
+
+    var set: std.AutoHashMap(u64, void) = .init(std.testing.allocator);
+    defer set.deinit();
+
+    for (0..5000) |_| {
+        const id = try flake.next();
+        const res = try set.getOrPut(id);
+
+        try std.testing.expect(!res.found_existing);
+    }
+}
+
+test "Snowflake_threaded" {}
